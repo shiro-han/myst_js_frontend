bae19d50d42c940f41566cf21422557a238247d6
shiro-han <shan99business@gmail.com>
2020-07-28 19:47:56 -0400

    some new functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const CORS_URL = 'https://cors-anywhere.herokuapp.com/'
 const API_URL = "https://api-v3.igdb.com/games"
-const RAILS_URL = "http://localhost:3000/games"
+const RAILS_URL = "http://localhost:3000/"
 const API_KEY = 'cfc722389b379cdc9ee497832c009ac3'
 
 const getGame = (id) => {
@@ -19,7 +19,7 @@ const getGame = (id) => {
     
     fetch(CORS_URL+API_URL, requestOptions)
     .then(resp => resp.json())
-    .then(json => console.log(json))
+    .then(json => {console.log(json[0]); renderGame(json[0]); postGameToRails(json[0])})
     .catch(error => console.log('error', error));
 }
 
@@ -104,9 +104,92 @@ const getPopularGames = () => {
 }
 
 const renderGame = (game) => {
-    
+    let div = document.createElement('div')
+    div.dataset.id = game.id
+    div.innerHTML = `
+    <h1>${game.name}</h1>
+    <h2>Console: ${platformsString(game.platforms)}</h2>
+    <p>${game.summary}</p>
+    <img src = ${imgURL(game.cover.url)}>
+    `
+    document.body.appendChild(div)
 }
 
-const postGameToCollection = () => {
+const platformsString = (array) => {
+    return array.map(platform => `${platform.name}`).join(', ')
+}
 
+const imgURL = (url, size = 'cover_big') => {
+    return 'http:' + url.replace('thumb', size)
+}
+
+const fetchRailsUsers = () => {
+    fetch (RAILS_URL + 'users')
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+}
+
+const postGameToRails = (gameObj) => {
+    let myHeaders = new Headers();
+    myHeaders.append('content-type', 'application/json');
+    myHeaders.append('accept', 'application/json')
+
+    let bodyObj = {
+        name: gameObj.name,
+        api_id: gameObj.id
+    }
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(bodyObj)
+    }
+
+    fetch(RAILS_URL + 'games', requestOptions)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+}
+
+const addGameToCollection = (userID, railsID) => {
+    let myHeaders = new Headers();
+    myHeaders.append('content-type', 'application/json');
+    myHeaders.append('accept', 'application/json')
+
+    let bodyObj = {
+        user_id: userID,
+        game_id: railsID
+    }
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(bodyObj)
+    }
+
+    fetch(RAILS_URL + 'user_games', requestOptions)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+}
+
+const registerUser = (userObj) => {
+    let myHeaders = new Headers();
+    myHeaders.append('content-type', 'application/json');
+    myHeaders.append('accept', 'application/json')
+
+    let bodyObj = {
+        username: userObj.username,
+        bio: userObj.bio,
+        email: userObj.email,
+        profile_pic: userObj.profile_pic
+    }
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(bodyObj)
+    }
+
+    fetch(RAILS_URL + 'users', requestOptions)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
 }
