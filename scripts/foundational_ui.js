ba02b290e56d5bdf23bf308cf458f6ecc820cab6
maxcroy1 <max.croy1@gmail.com>
2020-07-30 12:02:54 -0400

    nfiuuwenfw

--- scripts/foundational_ui.js
+++ scripts/foundational_ui.js
@@ -1,5 +1,5 @@
 const CORS_URL = 'https://damp-taiga-79758.herokuapp.com/'
-const RAILS_URL = "http://localhost:3000/users"
+const RAILS_URL = "http://localhost:3000"
 const API_KEY = 'cfc722389b379cdc9ee497832c009ac3'
 
 $(function(){
